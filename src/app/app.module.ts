b298a0ed4d9f87bef52695af6e973332420ff300
Rafa29 <raphael.amorim.goncalves@gmail.com>
2020-05-11 17:38:37 +0200

    add components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { TvShowListComponent } from './tv-show-list/tv-show-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MovieListComponent,
+    TvShowListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
