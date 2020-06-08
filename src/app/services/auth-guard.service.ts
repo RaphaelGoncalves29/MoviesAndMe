8a6ad3e72e740877bad6375886e17b8a169342f7
Rafa29 <raphael.amorim.goncalves@gmail.com>
2020-06-08 18:26:30 +0200

    update firebase import

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import firebase from '@firebase/app';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
